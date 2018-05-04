8cde8aa9496db9138e995fa147168ffdd061388d
CoryOp <crahman@wisc.edu>
2018-05-04 10:35:28 -0400

    Progress Save - Promises set up

--- js/script.js
+++ js/script.js
@@ -3,33 +3,36 @@
 
     // Start main JavaScript stream
     function main() {
-        //        apiPullReps("38.990875, -77.345688")
-        //        apiPullCoords("1601 fieldthorn drive reston va");
 
+        startPersonalizedGuide("invalid input")
+            //        startPersonalizedGuide("1415 Kamehameha IV Rd Honolulu HI")
 
     };
 
 
-
-
+    // Initialize variables
+    var apiUrlKey = "";
+    var repsData_personalizedGuide
+    var repsData_exploreMap
 
     // Declare functions
 
-    var apiUrlKey = "";
+    function startPersonalizedGuide(locationString) {
 
-    function apiPullCoords(locationString) {
-        //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+        try {
+            Promise.all([
+            apiPullReps(locationString),
+            apiPullCoords(locationString)
+        ]).then(function (promiseValues) {
 
-        var apiFullURLString = "https://maps.googleapis.com/maps/api/geocode/json?" +
-            "address=" + locationString +
-            "&key=" + apiUrlKey;
-        console.log(apiFullURLString);
-        $.ajax({
-            url: apiFullURLString,
-            success: function (returnObject) {
-                console.log(returnObject);
-            }
-        });
+                console.log(promiseValues)
+                    // unpack the loaded data into variables
+                    //var [csvData, jsonStates] = promiseValues
+
+            })
+        } catch {
+            console.log("error in promise.all")
+        }
     }
 
     function apiPullReps(locationString) {
@@ -37,14 +40,28 @@
             "representatives" +
             "?key=" + apiUrlKey +
             "&address=" + locationString
-        $.ajax({
-            url: apiFullURLString,
-            success: function (returnObject) {
-                console.log(returnObject);
-            }
-        });
+        return Promise.resolve($.ajax({
+                url: apiFullURLString,
+                //            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                //                console.log("XMLHttpRequest, textStatus, errorThrown")
+                //                console.log(XMLHttpRequest.status)
+                //            }
+            })
+            .catch(function (err) {
+                //                console.log(err)
+                //                console.log("test")
+            })
+        );
     }
 
+    function apiPullCoords(locationString) {
+        var apiFullURLString = "https://maps.googleapis.com/maps/api/geocode/json?" +
+            "address=" + locationString +
+            "&key=" + apiUrlKey;
+        return Promise.resolve($.ajax({
+            url: apiFullURLString
+        }));
+    }
 
 
     main()
